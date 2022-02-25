78a648f102fa20a42f411b7eef4af1bf958615c0
mrdLokman <merdjilokman@gmail.com>
2022-02-25 15:40:58 -0800

    add login with facebook button

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,20 @@
 
 
 const theme = {
-    primary: "#FE3C72",
+    primary: "#FD297B",
     secondary: "#FF5864",
+    gray: "#424242",
+    statusBarStyle: "light-content",
+    mainText:"#424242",
+}
+
+const icons = {
+    google: require("../../assets/google_icon.png"),
+    facebook: require("../../assets/facebook_icon.png"),
 }
 
 const images = {
     logo: require("../../assets/tinder_logo.png"),
 }
 
-export { theme, images };
+export { theme, images, icons };
--- src/screens/login_screen.js
+++ src/screens/login_screen.js
@@ -1,30 +1,44 @@
 import React from 'react'
 import { LinearGradient } from 'expo-linear-gradient';
-import { View, Text, ProgressBarAndroid, Image, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ProgressBarAndroid, Image, TouchableOpacity, StyleSheet, StatusBar } from 'react-native'
 import useAuth from '../hooks/useAuth'
-import { theme, images } from '../constants';
+import { theme, images, icons } from '../constants';
 
 const LoginScreen = () => {
 
     const { signinWithGoogle, loading } = useAuth();
 
     if(!loading)
-        return (
+        return (       
             <View style={styles.container}>
+                <StatusBar barStyle={theme.statusBarStyle} backgroundColor={'transparent'} translucent={true}/>
                 <LinearGradient colors={[theme.primary, theme.secondary]} style={styles.container}>
                     <View style={styles.content}>
-                        <Image source={images.logo} style={styles.background}/>
-                    
-                        <TouchableOpacity 
-                            style={styles.loginButton}
-                            onPress={signinWithGoogle}
-                        >
-                            <Text style={styles.loginText}>Sign-in with google</Text>
-                        </TouchableOpacity>
+                        <Image 
+                            source={images.logo}
+                            style={styles.background}
+                        />
+
+                        <View style={styles.buttonsSection}>
+                            <TouchableOpacity 
+                                style={styles.loginButton}
+                                onPress={signinWithGoogle}
+                            >
+                                <Image source={icons.google} style={styles.loginIcon}/>
+                                <Text style={styles.loginText}>Login with google</Text>
+                            </TouchableOpacity>
+                            <View style={styles.spacer}/>
+                            <TouchableOpacity 
+                                style={styles.loginButton}
+                                onPress={()=>console.log("Not implemented yet!")}
+                            >
+                                <Image source={icons.facebook} style={styles.loginIcon}/>
+                                <Text style={styles.loginText}>Login with facebook</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </LinearGradient>
-            </View>
-            
+            </View>          
         );
     else
         return(
@@ -48,19 +62,31 @@ const styles = StyleSheet.create({
         alignItems:"center",
         justifyContent:"space-around"
     },
+    buttonsSection:{
+        marginHorizontal: "20%",
+    },
     loginButton:{
         backgroundColor:"white",
         height: 40,
-        width:"50%",
+        width: 200,
         marginHorizontal:"25%",
         borderRadius:15,
+        flexDirection: "row",
         alignItems:"center",
         justifyContent:"center"
     },
     loginText:{
         textAlign:"center",
         fontWeight:"700",
-        color:"#424242"
+        color:theme.mainText,
+    },
+    loginIcon:{
+        width:20,
+        height:20,
+        marginHorizontal:10,
+    },
+    spacer:{
+        height:5,
     }
 });
 
